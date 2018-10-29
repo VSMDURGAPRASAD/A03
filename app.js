28e53a1f582b59a573644ff75732bf17d3dabee7
SAI MANIKANTA DURGA PRASAD VINUKONDA <sai1996.vinukonda@gmail.com>
2018-10-28 19:07:48 -0500

    test

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var http = require('http').Server(app);
+var nodemailer = require('nodemailer');
+const bodyParser = require("body-parser");
+
+app.use(express.static(path.join(__dirname,'Public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'Views'));
+app.set('view engine', 'ejs');
 
 app.all("/",function(req, res){
     res.render('index')
@@ -14,10 +23,34 @@ app.all("/yourchoice",function(req,res){
     res.render('yourchoice')
 })
 
+app.post("/mail", function(req, res){
+    console.log(req.body.email1)
+    
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'vinukonda.a03@gmail.com',
+          pass: 'Project12345678'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'vinukonda.a03@gmail.com',
+        to: req.body.email1,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      res.render('Contact');
+})
 
-app.use(express.static(path.join(__dirname,'Public')));
-app.set('views', path.join(__dirname, 'Views'));
-app.set('view engine', 'ejs');
 
 http.listen(8083, function(){
 console.log("App is listening on http://127.0.0.1:8083/")
