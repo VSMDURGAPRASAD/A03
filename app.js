1c6931f0fb65945dff7bb8108ea78c608d4c6577
SAI MANIKANTA DURGA PRASAD VINUKONDA <sai1996.vinukonda@gmail.com>
2018-10-28 19:15:18 -0500

    test

--- app.js
+++ app.js
@@ -5,54 +5,54 @@ var http = require('http').Server(app);
 var nodemailer = require('nodemailer');
 const bodyParser = require("body-parser");
 
-app.use(express.static(path.join(__dirname,'Public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, 'Views'));
 app.set('view engine', 'ejs');
 
-app.all("/",function(req, res){
+app.get("/", function (req, res) {
     res.render('index')
 
 })
-app.all("/contact",function(req,res){
+app.get("/contact", function (req, res) {
     res.render('Contact.ejs');
 })
-app.all("/yourchoice",function(req,res){
+app.get("/yourchoice", function (req, res) {
     res.render('yourchoice')
 })
 
-app.post("/mail", function(req, res){
+app.post("/mail", function (req, res) {
     console.log(req.body.email1)
-    
+
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-          user: 'vinukonda.a03@gmail.com',
-          pass: 'Project12345678'
+            user: 'vinukonda.a03@gmail.com',
+            pass: 'Project12345678'
         }
-      });
-      
-      var mailOptions = {
+    });
+
+    var mailOptions = {
         from: 'vinukonda.a03@gmail.com',
         to: req.body.email1,
         subject: 'Sending Email using Node.js',
         text: 'That was easy!'
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-          console.log(error);
+            console.log(error);
         } else {
-          console.log('Email sent: ' + info.response);
+            console.log('Email sent: ' + info.response);
         }
-      });
-      res.render('Contact');
+    });
+    res.render('Contact');
 })
 
 
-http.listen(process.event.PORT || 8083, function(){
-console.log("App is listening on http://127.0.0.1:8083/")
+http.listen(process.env.PORT || 8083, function () {
+    console.log("App is listening on http://127.0.0.1:8083/")
 }
-    )
+)
